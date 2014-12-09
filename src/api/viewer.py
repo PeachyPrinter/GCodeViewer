fce7ca711710164bfc37028740214c90bab139d9
james townley <james@townley.ca>
2014-12-09 11:21:51 -0600

    Started working on api test and impl

--- src/api/viewer.py
+++ src/api/viewer.py
@@ -1,11 +1,17 @@
-
+from infrastructure.asynchronous_gcode_reader import AsynchronousGcodeReader 
 
 class Viewer(object):
     def __init__(self):
         pass
 
-    def load_gcode(self, file, call_back = None):
-        pass
+    def load_gcode(self, afile, call_back = None):
+        AsynchronousGcodeReader(afile,self.load_gcode_call_back,self.load_gcode_complete).start()
 
     def get_layers(self,start,end,skip):
+        pass
+
+    def load_gcode_call_back(self):
+        pass
+
+    def load_gcode_complete(self):
         pass
--- test/api_test/viewer_test.py
+++ test/api_test/viewer_test.py
@@ -8,9 +8,18 @@ sys.path.insert(0,os.path.join(os.path.dirname(__file__), '..', '..','src'))
 
 from api.viewer import Viewer
 
+@patch('api.viewer.AsynchronousGcodeReader')
 class ViewerTest(unittest.TestCase):
-    def test_(self):
-        pass
+    def test_load_gcode_should_read_gcode(self,mock_AsynchronousGcodeReader):
+        viewer = Viewer()
+        test_file = 'test_file.gcode'
+        mock_asynchronous_gcode_reader = mock_AsynchronousGcodeReader.return_value
+        
+        viewer.load_gcode(test_file)
+
+        self.assertEquals(test_file, mock_AsynchronousGcodeReader.call_args_list[0][0][0])
+        mock_asynchronous_gcode_reader.start.assert_called_with()
+
 
 if __name__ == '__main__':
     unittest.main()
