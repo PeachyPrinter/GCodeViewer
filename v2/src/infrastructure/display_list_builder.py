418532d4c10ccfc93fe5ee80f23dfb3d38910196
james townley <james@townley.ca>
2015-01-09 13:45:27 -0600

    Adding more details around drawing lines

--- v2/src/infrastructure/display_list_builder.py
+++ v2/src/infrastructure/display_list_builder.py
@@ -1,11 +1,18 @@
 import OpenGL.GL as gl
-import OpenGL.GLUT as glut
+
+from domain.point import Point
 
 
 class DisplayListBuilder(object):
     def get_list_id(self, points):
+        last_point = Point(0.0, 0.0, 0.0, False)
         display_list_id = gl.glGenLists(1)
         gl.glNewList(display_list_id, gl.GL_COMPILE)
+        gl.glBegin(gl.GL_LINES)
+        gl.glVertex3f(*last_point.xzy)
+        for point in points:
+            gl.glVertex3f(*point.xzy)
+        gl.glEnd()
         gl.glEndList()
         return display_list_id
 
--- v2/test/infrastructure_test/display_list_builder_test.py
+++ v2/test/infrastructure_test/display_list_builder_test.py
@@ -1,10 +1,8 @@
 import unittest
 import os
 import sys
-from mock import patch, MagicMock
-import numpy as np
+from mock import patch, call
 import OpenGL.GL as gl
-import OpenGL.GLUT as glut
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
@@ -12,11 +10,11 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
 from infrastructure.display_list_builder import DisplayListBuilder
 from domain.point import Point
 
+
 @patch('infrastructure.display_list_builder.gl')
-@patch('infrastructure.display_list_builder.glut')
 class DisplayListBuilderTest(unittest.TestCase):
 
-    def test_get_list_id_should_create_a_new_display_list(self, mock_glut, mock_gl):
+    def test_get_list_id_should_create_a_new_display_list(self, mock_gl):
         expected_list_id = gl.GLuint(1)
 
         mock_gl.glGenLists.return_value = expected_list_id
@@ -28,6 +26,19 @@ class DisplayListBuilderTest(unittest.TestCase):
         self.assertEquals(expected_list_id, list_id)
         mock_gl.glNewList.assert_called_with(expected_list_id, gl.GL_COMPILE)
         mock_gl.glEndList.assert_called_with()
+
+    def test_get_list_should_create_gl_lines(self, mock_gl):
+        mock_gl.GL_LINES = gl.GL_LINES
+        x, y, z = 0.5, 0.6, 0.7
+        points = [Point(x, y, z, False)]
+        dlb = DisplayListBuilder()
+
+        dlb.get_list_id(points)
+
+        mock_gl.glBegin.assert_called_with(gl.GL_LINES)
+        mock_gl.glVertex3f.assert_has_calls([call(0.0, 0.0, 0.0), call(x, z, y)])
+        mock_gl.glEnd.assert_called_with()
+
 
 if __name__ == '__main__':
     unittest.main()
